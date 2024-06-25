cec06062e7a6c598d84e85100d3d712f7ce04fee
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-06-25 12:23:38 +0200

    added webrebirth logo

--- src/app/(contact)/Contact.tsx
+++ src/app/(contact)/Contact.tsx
@@ -5,6 +5,7 @@ import ContactForm from "./ContactForm";
 import discord_2 from "/public/assets/images/svgs/discord_2.svg";
 import facebook from "/public/assets/images/svgs/facebook.svg";
 import twitch from "/public/assets/images/svgs/twitch.svg";
+import webrebirth_logo from "/public/assets/logo/webrebirth_logo.png";
 
 const contacts = [
   {
@@ -64,6 +65,16 @@ const Contact = () => {
                 {contact.name}
               </Link>
             ))}
+          </div>
+          <div className="flex flex-wrap justify-center items-center gap-3 w-fit">
+            <p className="text-center">A weboldalt készítette</p>
+            <Link
+              className="block mx-auto w-fit hover:opacity-50 transition-opacity"
+              href="https://webrebirth.hu/"
+              target="_blank"
+            >
+              <Image height={35} src={webrebirth_logo} alt="webrebirth logo" />
+            </Link>
           </div>
         </div>
         <ContactForm />
