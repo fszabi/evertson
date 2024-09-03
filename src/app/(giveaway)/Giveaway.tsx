bf5bf261abae87660c305996e69fc826b163a348
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-09-03 18:55:11 +0200

    giveaway

--- src/app/(giveaway)/Giveaway.tsx
+++ src/app/(giveaway)/Giveaway.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Giveaway = () => {
   return (
     <section id="nyeremenyjatek" className="py-28">
-      <div className="max-w-7xl px-6 lg:px-8 mx-auto space-y-20">
+      <div className="max-w-7xl px-6 lg:px-8 mx-auto space-y-10">
         <h3>3D AK-47 Gold Arabesque Nyereményjáték</h3>
         <div className="w-[500px]">
           <Link
