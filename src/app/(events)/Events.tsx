db95d336ba0bb28a6b2f98b95006b61a156b61c1
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-10-24 16:09:33 +0200

    events

--- src/app/(events)/Events.tsx
+++ src/app/(events)/Events.tsx
@@ -1,11 +1,6 @@
-// type StatusType = "Completed" | "Error";
-
 type StatusType = "Hamarosan" | "Folyamatban" | "Befejeződött";
 
 const statuses: { [key in StatusType]: string } = {
-  // Completed: "text-green-400 bg-green-400/10",
-  // Error: "text-rose-400 bg-rose-400/10",
-
   Hamarosan: "text-blue-400 bg-blue-400/10",
   Folyamatban: "text-yellow-400 bg-yellow-400/10",
   Befejeződött: "text-green-400 bg-green-400/10",
@@ -15,49 +10,81 @@ const activityItems = [
   {
     id: 1,
     type: "Event típus",
-    prize: "Nyeremény",
-    status: "Folyamatban" as StatusType,
-    server: "AWP",
-    time: "22:06:37",
-    dateTime: "2023-01-23T11:00",
+    firstPlacePrize: "1. hely - 1 hetes VIP 1v1 szerveren",
+    secondPlacePrize:
+      "2. hely - StatTrak™ XM1014 | Teclu Burner (Battle-Scarred)",
+    thirdPlacePrize: "3. hely - Charm | Baby's AK",
+    status: "" as StatusType,
+    server: "1v1",
+    time: "2024 október 27 19:00",
+    dateTime: "2024-10-27T19:00",
   },
   {
     id: 2,
     type: "Event típus",
-    prize: "Nyeremény",
-    status: "Hamarosan" as StatusType,
-    server: "PUB",
-    time: "16:02:44",
-    dateTime: "2023-01-23T09:00",
+    firstPlacePrize: "1. hely - 1 hetes VIP AWP szerveren",
+    secondPlacePrize: "2. hely - Charm | Lil' SAS",
+    thirdPlacePrize: "3. hely - Sticker | Boom (Holo)",
+    status: "" as StatusType,
+    server: "AWP",
+    time: "2024 november 3 19:00",
+    dateTime: "2024-11-03T19:00",
   },
   {
     id: 3,
     type: "Event típus",
-    prize: "Nyeremény",
-    status: "Befejeződött" as StatusType,
+    firstPlacePrize: "1. hely - 1 hetes VIP 1v1 szerveren",
+    secondPlacePrize: "2. hely - P90 | Vent Rush (Field-Tested)",
+    thirdPlacePrize: "3. hely - MAC-10 | Allure (Field-Tested)",
+    status: "" as StatusType,
     server: "1v1",
-    time: "10:16:52",
-    dateTime: "2023-01-23T00:00",
+    time: "2024 november 10 19:00",
+    dateTime: "2024-11-10T10:00",
   },
   {
     id: 4,
     type: "Event típus",
-    prize: "Nyeremény",
-    status: "Befejeződött" as StatusType,
+    firstPlacePrize: "1. hely - 1 hetes VIP AWP szerveren",
+    secondPlacePrize: "2. hely - USP-S | Alpine Camo (Minimal Wear)",
+    thirdPlacePrize: "3. hely - SSG 08 | Fever Dream (Field-Tested)",
+    status: "" as StatusType,
     server: "AWP",
-    time: "02:05:28",
-    dateTime: "2023-01-21T13:00",
+    time: "2024 november 17 19:00",
+    dateTime: "2024-11-17T19:00",
   },
   {
     id: 5,
     type: "Event típus",
-    prize: "Nyeremény",
-    status: "Hamarosan" as StatusType,
-    server: "AWP",
-    time: "01:01:04",
-    dateTime: "2023-01-18T12:34",
+    firstPlacePrize: "1. hely - 1 hetes VIP 1v1 szerveren",
+    secondPlacePrize: "2. hely - M4A1-S | Night Terror (Battle-Scarred)",
+    thirdPlacePrize: "3. hely - R8 Revolver | Banana Cannon (Well-Worn)",
+    status: "" as StatusType,
+    server: "1v1",
+    time: "2024 november 24 19:00",
+    dateTime: "2024-11-24T19:00",
   },
 ];
+
+const now = new Date();
+
+const updatedActivityItems = activityItems.map((item) => {
+  const eventDate = new Date(item.dateTime);
+
+  if (eventDate > now) {
+    item.status = "Hamarosan";
+  } else if (
+    eventDate.toDateString() === now.toDateString() &&
+    eventDate.getTime() <= now.getTime() &&
+    now.getTime() <= eventDate.getTime() + 3600000
+  ) {
+    // Check if the event is happening today and within the current time
+    item.status = "Folyamatban";
+  } else {
+    item.status = "Befejeződött";
+  }
+
+  return item;
+});
 
 function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(" ");
@@ -79,15 +106,21 @@ const Events = () => {
         </colgroup>
         <thead className="border-b border-white/10 text-sm leading-6 text-white">
           <tr>
-            <th
+            {/* <th
               scope="col"
               className="hidden py-2 pl-4 pr-8 font-semibold min-[370px]:table-cell sm:pl-6 lg:pl-8"
             >
               Típus
+            </th> */}
+            <th
+              scope="col"
+              className="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8 lg:pr-20"
+            >
+              Szerver
             </th>
             <th
               scope="col"
-              className="hidden py-2 pl-0 pr-8 font-semibold sm:table-cell"
+              className="hidden py-2 pl-0 pr-8 font-semibold lg:table-cell"
             >
               Nyeremény
             </th>
@@ -99,40 +132,37 @@ const Events = () => {
             </th>
             <th
               scope="col"
-              className="py-2 pl-0 max-[369px]:pl-4 pr-8 font-semibold lg:pr-20"
-            >
-              Szerver
-            </th>
-            <th
-              scope="col"
               className="py-2 pl-0 pr-4 text-right font-semibold sm:pr-6 lg:pr-8"
             >
-              Hátralévő idő
+              Időpont
             </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-white/5">
-          {activityItems.map((item) => (
+          {updatedActivityItems.map((item) => (
             <tr key={item.id}>
-              <td className="hidden py-4 pl-4 pr-8 min-[370px]:table-cell sm:pl-6 lg:pl-8">
-                {/* <div className="truncate text-sm font-medium leading-6 text-white">
-                  {item.type}
-                </div> */}
-
+              {/* <td className="hidden py-4 pl-4 pr-8 min-[370px]:table-cell sm:pl-6 lg:pl-8">
                 <div className="rounded-md w-fit bg-gray-700/40 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-white/10">
                   {item.type}
                 </div>
+              </td> */}
+              <td className="py-4 pr-0 pl-4 text-sm leading-6 text-gray-400 sm:pr-6 sm:pl-6 lg:pl-8">
+                {item.server}
               </td>
-              <td className="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
+              <td className="hidden py-4 pl-0 pr-4 lg:table-cell sm:pr-8">
                 <div className="font-mono text-sm leading-6 text-gray-400">
-                  {item.prize}
+                  <p>{item.firstPlacePrize}</p>
+                  <p>{item.secondPlacePrize}</p>
+                  <p>{item.thirdPlacePrize}</p>
                 </div>
               </td>
               <td className="hidden py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20 md:table-cell">
                 <div className="flex items-center justify-end gap-x-2 sm:justify-start">
                   <div
                     className={classNames(
-                      statuses[item.status],
+                      item.dateTime > new Date().toISOString()
+                        ? statuses[item.status]
+                        : statuses[item.status],
                       "flex-none rounded-full p-1"
                     )}
                   >
@@ -140,9 +170,6 @@ const Events = () => {
                   </div>
                   <div className="text-white">{item.status}</div>
                 </div>
-              </td>
-              <td className="py-4 pl-0 max-[369px]:pl-4 pr-8 text-sm leading-6 text-gray-400 lg:pr-20">
-                {item.server}
               </td>
               <td className="py-4 pl-0 pr-4 text-right text-sm leading-6 text-gray-400 sm:pr-6 lg:pr-8">
                 <time dateTime={item.dateTime}>{item.time}</time>
