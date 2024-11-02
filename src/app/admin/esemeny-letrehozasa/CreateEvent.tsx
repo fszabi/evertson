7e21ed41632169111462f623bcbefa2bfb8e304b
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-11-02 18:08:53 +0100

    fix

--- src/app/admin/esemeny-letrehozasa/CreateEvent.tsx
+++ src/app/admin/esemeny-letrehozasa/CreateEvent.tsx
@@ -45,7 +45,7 @@ const CreateEvent = () => {
         />
         <input
           className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset bg-zinc-800 ring-zinc-700 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-red-600 md:text-sm md:leading-6 transition-shadow"
-          type="datetime-local"
+          type="date"
           lang="hu-HU"
           id="deadline"
           name="deadline"
