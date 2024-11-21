ddb9e8e6cda65ba18f8fb50413a7c1bc66d7c91f
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-11-21 12:19:25 +0100

    fix

--- src/app/(header)/Header.tsx
+++ src/app/(header)/Header.tsx
@@ -88,7 +88,6 @@ const Header = () => {
             <Link
               key={link.name}
               href={link.href}
-              scroll={false}
               className="font-semibold hover:opacity-80 transition-opacity"
             >
               {link.name}
@@ -96,64 +95,55 @@ const Header = () => {
           ))}
         </div>
       </nav>
-      <Transition appear show={mobileMenuOpen}>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          onClose={() => setMobileMenuOpen(false)}
+
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={() => setMobileMenuOpen(false)}
+      >
+        <div className="fixed inset-0 bg-black/25" />
+
+        <TransitionChild
+          enter="ease-out duration-200"
+          enterFrom="opacity-0 translate-x-full"
+          enterTo="opacity-100 translate-x-0"
+          leave="ease-out duration-200"
+          leaveFrom="opacity-100 translate-x-0"
+          leaveTo="opacity-0 translate-x-full"
         >
-          <TransitionChild
-            enter="ease-out duration-200"
-            enterFrom="opacity-0 translate-x-full"
-            enterTo="opacity-100 translate-x-0"
-            leave="ease-out duration-200"
-            leaveFrom="opacity-100 translate-x-0"
-            leaveTo="opacity-0 translate-x-full"
-          >
-            <div className="fixed inset-0 bg-black/25" />
-          </TransitionChild>
-          <TransitionChild
-            enter="ease-out duration-200"
-            enterFrom="opacity-0 translate-x-full"
-            enterTo="opacity-100 translate-x-0"
-            leave="ease-out duration-200"
-            leaveFrom="opacity-100 translate-x-0"
-            leaveTo="opacity-0 translate-x-full"
-          >
-            <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-zinc-900 py-5 px-8 sm:max-w-sm sm:border-l sm:border-l-zinc-800">
-              <div className="flex items-center justify-between">
-                <div>
-                  <span className="sr-only">Evertson Logo</span>
-                  <Image
-                    className="h-10 w-10 rounded-lg"
-                    src={logo}
-                    alt="evertson logo"
-                  />
-                </div>
-                <button type="button" onClick={() => setMobileMenuOpen(false)}>
-                  <span className="sr-only">Navigációs menü bezárása</span>
-                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-zinc-900 py-5 px-8 sm:max-w-sm sm:border-l sm:border-l-zinc-800">
+            <div className="flex items-center justify-between">
+              <div>
+                <span className="sr-only">Evertson Logo</span>
+                <Image
+                  className="h-10 w-10 rounded-lg"
+                  src={logo}
+                  alt="evertson logo"
+                />
               </div>
-              <div className="mt-6 flow-root">
-                <div className="space-y-2">
-                  {links.map((link) => (
-                    <Link
-                      key={link.name}
-                      href={link.href}
-                      scroll={false}
-                      onClick={() => setMobileMenuOpen(false)}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-zinc-800 transition-colors"
-                    >
-                      {link.name}
-                    </Link>
-                  ))}
-                </div>
+              <button type="button" onClick={() => setMobileMenuOpen(false)}>
+                <span className="sr-only">Navigációs menü bezárása</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="space-y-2">
+                {links.map((link) => (
+                  <Link
+                    key={link.name}
+                    href={link.href}
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-zinc-800 transition-colors"
+                  >
+                    {link.name}
+                  </Link>
+                ))}
               </div>
-            </DialogPanel>
-          </TransitionChild>
-        </Dialog>
-      </Transition>
+            </div>
+          </DialogPanel>
+        </TransitionChild>
+      </Dialog>
     </header>
   );
 };
