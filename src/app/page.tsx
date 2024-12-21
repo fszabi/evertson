fb1afabcec5760bcde29551976c3fba170d2ea36
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-12-21 20:03:14 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react";
 import Contact from "./(contact)/Contact";
 import { Faqs } from "./(faqs)/Faqs";
 import Header from "./(header)/Header";
 import Hero from "./(hero)/Hero";
 import Sites from "./(sites)/Sites";
 import Blog from "./Blog";
+import Loading from "./Loading";
 import ScrollArrow from "./ScrollArrow";
 import Services from "./Services";
 
@@ -17,7 +19,7 @@ export default async function Home() {
   await getData();
 
   return (
-    <>
+    <Suspense fallback={<Loading />}>
       <Header />
       <Hero />
       <Services />
@@ -26,6 +28,6 @@ export default async function Home() {
       <Contact />
       <Faqs />
       <ScrollArrow />
-    </>
+    </Suspense>
   );
 }
