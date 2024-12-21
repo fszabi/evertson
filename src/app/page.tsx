bcc3181df288d3baf2ed1d4a068d195abd8cfa96
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-12-21 20:01:57 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,13 @@ import Services from "./Services";
 
 export const dynamic = "force-dynamic";
 
+async function getData() {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+}
+
 export default async function Home() {
+  await getData();
+
   return (
     <>
       <Header />
