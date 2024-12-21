457b993ffc5cc8851753a0c1b4659b293583d8ea
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-12-21 19:22:08 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from "react";
-import { promisify } from "util";
 import Contact from "./(contact)/Contact";
 import { Faqs } from "./(faqs)/Faqs";
 import Header from "./(header)/Header";
@@ -8,6 +6,8 @@ import Sites from "./(sites)/Sites";
 import Blog from "./Blog";
 import ScrollArrow from "./ScrollArrow";
 import Services from "./Services";
+
+export const dynamic = "force-dynamic";
 
 async function getData() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
